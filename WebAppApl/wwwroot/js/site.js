d89e7ae9f1f169f7c3d6ddd166874d0168a5cea4
Nikita Sazonov <sazonovn2018@gmail.com>
2022-11-29 13:01:41 -0700

    Commit

---
+++ WebAppApl/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function NewColor() {
+    document.getElementById('helo_world').style.color = 'red';
+}
+
+function OldColor() {
+    document.getElementById('helo_world').style.color = 'silver';
+}
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
